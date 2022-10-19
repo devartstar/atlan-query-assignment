4082cf377cbe96796f1ce36edb9a0a2d89303456
devartstar <devjitc1@gmail.com>
2022-10-19 11:48:50 +0530

    feat: added codemirror to project

--- src/components/views/EditorPage/Editor.vue
+++ src/components/views/EditorPage/Editor.vue
@@ -1,9 +1,11 @@
 <template>
-  <div
-    class="lg:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-[36rem] bg-black"
-  ></div>
+  <div class="lg:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-[36rem]">
+    <code-mirror v-model="code" basic />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
-
-<style scoped></style>
+<script setup lang="ts">
+import { ref } from "vue";
+import CodeMirror from "vue-codemirror6";
+const code = ref("Some code");
+</script>
